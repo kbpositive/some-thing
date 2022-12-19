89eb0c7a17f163c8d455432a00e8416310f57402
Kadeem Baldwin <Kadbaldwin@gmail.com>
2022-12-18 22:37:16 -0500

    added gifuct-js decoding

--- some-thing/src/app/app.component.ts
+++ some-thing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { parseGIF, decompressFrames } from 'gifuct-js';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,15 @@ export class AppComponent {
     const reader = new FileReader();
     reader.onload = (e: any) => {
       this.Image = e.target.result
+      var promisedGif = fetch(e.target.result)
+        .then(resp => resp.arrayBuffer())
+        .then(buff => {
+          var gif = parseGIF(buff)
+          var frames = decompressFrames(gif, true)
+          return gif;
+        });
+
+      console.log(promisedGif)
     };
     reader.readAsDataURL(event.target.files[0]);
   }
